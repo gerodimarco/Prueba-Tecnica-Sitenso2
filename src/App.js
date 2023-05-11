9492c035ec83b967653d3fe6e6f0a2eea2f22d91
Geronimo Dimarco <unzagageronimo@gmail.com>
2023-05-10 23:48:00 -0300

    Estableciendo las routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter, Link } from 'react-router-dom';
 import Navbar from "./components/NavBar/Navbar";
 import './App.css';
-import Comics from "./components/Comics/Comics";
+import { BrowserRouter as Router,
+Routes,Route} from "react-router-dom";
 
 const Home = () => {
   const [characters, setCharacters] = useState([]);
@@ -32,15 +32,41 @@ const Home = () => {
 
 
   return (
+    <Router>
+      <Navbar />
     <div className="App">
-    <Navbar />
+    <Routes>
+      {/* se utiliza el element y no un exact para traer el componente directo, ya que me tira error */}
+        <Route path ="/comics" element ={
+              <div className="card-container col-md-4 ">
+              {comics && comics.map((comic) => (
+                  <div className="card" key={comic.id}>
+                    <h2>{comic.title}</h2>
+                    <img
+                    src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+                    alt={comic.title} />
+                  </div>
+        ))}
+              </div>
+        }>
+        </Route>
+        <Route path ="/characters" element ={
+              <div className="card-container col-md-4">
+              {characters && characters.map((character) => (
+                  <div className="card" key={character.id}>
+                      <h2>{character.name}</h2>
+                      <img
+                          src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                          alt={character.name} />
+                  </div>
+              ))}
+          </div>
+        }>
+
+        </Route>
+      </Routes>
     <div className="container col-md-8">
-    <BrowserRouter>
-      <Switch>
-        <Route path="./components/Comics/Comics.jsx" component="Comics.jsx" />
-      </Switch>
-    </BrowserRouter>
-      <div className="card" onClick={() => history.push('/Comics.jsx')} >
+      <div className="card" >
       <h2>COMICS</h2>
         <img src="https://www.tebeosfera.com/T3content/img/T3_numeros/a/n/mmspidermanv1_038_09_pan.jpg" alt="/"/>
       </div>
@@ -50,6 +76,7 @@ const Home = () => {
       </div>
     </div>
   </div>
+  </Router>
 );
 };
 
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+
+
     return (
         <div className='navbar'>
+            
+            
             <h1>MARVEL</h1>
+{/* 
+            <div class="search">
+            <input type="text" placeholder="Buscar"> </input>
+            <button type="submit">Buscar</button>
+            </div> */}
 
         </div>
+
     )
 }
 
